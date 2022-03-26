aeb8693e51733df99cc8690bebe3e9bbf2e5476a
MMoltira <MMoltira@example.com>
2022-03-26 10:32:56 +0700

    python2

---
+++ python2.py
@@ -0,0 +1,30 @@
+import argparse #สำหรับ รับ input จากภายนอก
+import subprocess #สำหรับรัน terminal command 
+
+#import flask # สำหรับทำ web app และ web service api
+
+
+def print_other():
+    print('something else')
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='test program to learn about argparse and subprocess ')
+    parser.add_argument(
+        'm',
+        type=int,
+        help='value of M positional argument')
+    parser.add_argument(
+        '--x',
+        type=int,
+        help='value of x')
+    parser.add_argument(
+        '--yval',
+        type=int,
+        default=3,
+        help='value of y')
+    args = parser.parse_args()
+
+    x = args.x
+    y = args.yval
+    print(f'M={args.m}')
+    print(f'calculate {x} x {y} = {x*y}')
